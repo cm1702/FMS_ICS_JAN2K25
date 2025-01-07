2d3f7a79093ad11f15e00ab0a4e0ac759fea99b5
Ajay Sairam <ajaysa@infinite.com>
2025-01-07 14:22:15 +0530

    setup1

--- FMS_ICS/FMS_ICS/Controllers/LoginController.cs
+++ FMS_ICS/FMS_ICS/Controllers/LoginController.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-public class Class1
+namespace FMS_ICS.Controllers
 {
-	public Class1()
-	{
-	}
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+    }
 }
